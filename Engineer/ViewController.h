edc816eab04783e7c4caa414f23f16cbc07c2a7d
nguyennghia <nghia@vulcanlabs.co>
2020-12-04 14:12:00 +0700

    first commit

--- Engineer/ViewController.h
+++ Engineer/ViewController.h
@@ -6,6 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
 
 @interface ViewController : UIViewController
 
--- Engineer/ViewController.m
+++ Engineer/ViewController.m
@@ -6,6 +6,11 @@
 //
 
 #import "ViewController.h"
+#include <Foundation/Foundation.h>
+#include <dlfcn.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
+#import <CydiaSubstrate/CydiaSubstrate.h>
 
 @interface ViewController ()
 
@@ -13,10 +18,65 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+NSString *(*oldDescription)(id class, SEL _cmd);
+NSString *newDescription(id class, SEL _cmd) {
+    NSString *description = (*oldDescription)(class, _cmd);
+    description = [description stringByAppendingString:@"Test!"];
+    return description;
 }
 
+void (*originSetFrame)(id class, SEL _cmd, CGRect frame);
+void replaceSetFrame(id class, SEL _cmd, CGRect frame) {
+    NSLog(@"Set frame");
+    originSetFrame(class, _cmd, frame);
+}
+
+id (*originValueForKey)(id class, SEL _cmd, NSString* key);
+id replaceValueForKey(id class, SEL _cmd, NSString* key) {
+    NSLog(@"valueForKey");
+    return originValueForKey(class, _cmd, key);
+}
+
+void (*originSetBackgroundColor)(id class, SEL _cmd, UIColor* color);
+void replaceSetBackgroundColor(id class, SEL _cmd, UIColor* color) {
+    NSLog(@"setBackgroundColor");
+    originSetBackgroundColor(class, _cmd, [UIColor purpleColor]);
+};
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    MSHookMessageEx([UIView class],
+                    @selector(setFrame:),
+                    &replaceSetFrame,
+                    &originSetFrame);
+    
+    MSHookMessageEx([UIApplication class],
+                    @selector(valueForKey:),
+                    &replaceValueForKey,
+                    &originValueForKey);
+    
+    MSHookMessageEx([UIView class],
+                    @selector(setBackgroundColor:),
+                    &replaceSetBackgroundColor,
+                    &originSetBackgroundColor);
+    
+    UIView *childView = [UIView new];
+    [childView setFrame:CGRectMake(0, 0, 100, 100)];
+    [[[UIApplication sharedApplication] valueForKey:@"statusBar"] setBackgroundColor:[UIColor greenColor]];
+    
+    [self.view setBackgroundColor:[UIColor yellowColor]];
+}
+
+
+
+//static mach_port_t getFrontMostAppPort() {
+//
+//    if([frontmostApp length] == 0 || locked)
+//        return GSGetPurpleSystemEventPort();
+//    else
+//        return GSCopyPurpleNamedPort(appId);
+//}
 
 @end
